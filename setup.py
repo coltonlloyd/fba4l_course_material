055d66e311bc7ecc7176c4406d12d8cdf75388c2
Colton Lloyd <coltonlloyd@Coltons-MBP-2.attlocal.net>
2023-02-11 10:20:57 -0800

    test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(name="fba4l_course_material",
+      version="",
+      author="Colton Lloyd",
+      url="https://github.com/coltonlloyd/fba4l_course_material",
+      packages=find_packages(),
+      )
+ 
